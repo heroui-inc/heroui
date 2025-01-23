9c09dfafe951918483012b0eb193fbd5590a6c39
ruchiket100 <ruchiketborse1@gmail.com>
2025-01-23 18:42:19 +0530

    fix(date-range-picker): improve responsiveness

--- packages/core/theme/src/components/date-input.ts
+++ packages/core/theme/src/components/date-input.ts
@@ -24,6 +24,7 @@ const dateInput = tv({
     input: "flex h-full gap-x-0.5 w-full font-normal",
     innerWrapper: [
       "flex items-center text-default-400 w-full gap-x-2 h-6",
+      "overflow-y-hidden overflow-x-auto scrollbar-hide",
       // isInValid=true
       "group-data-[invalid=true]:text-danger",
     ], // this wraps the input and the start/end content
