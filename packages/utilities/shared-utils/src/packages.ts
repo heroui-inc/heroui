7774dcab751c789403e2ed2d887beecf268c0f2f
WK Wong <wingkwong.code@gmail.com>
2025-01-26 21:04:24 +0800

    refactor: remove from shared-utils

--- packages/utilities/shared-utils/src/packages.ts
+++
@@ -1,6 +0,0 @@
-// re-exporting from external packages to ensure that
-// users to use the exact same version as the HeroUI components do
-// to avoid incompatibility issues
-
-// @internationalized/date"
-export * from "@internationalized/date";
