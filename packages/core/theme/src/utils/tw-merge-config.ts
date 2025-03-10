fb69ebc869db8a50ef67659147be37c8ebc86c5f
winches <329487092@qq.com>
2025-03-10 23:19:11 +0800

    fix: adapt tailwind merge config

--- packages/core/theme/src/utils/tw-merge-config.ts
+++ packages/core/theme/src/utils/tw-merge-config.ts
@@ -1,24 +1,22 @@
 export const COMMON_UNITS = ["small", "medium", "large"];
 
 export const twMergeConfig = {
-  extend: {
-    theme: {
-      spacing: ["divider"],
-      radius: COMMON_UNITS,
-    },
-    classGroups: {
-      shadow: [{shadow: COMMON_UNITS}],
-      opacity: [{opacity: ["disabled"]}],
-      "font-size": [{text: ["tiny", ...COMMON_UNITS]}],
-      "border-w": [{border: COMMON_UNITS}],
-      "bg-image": [
-        "bg-stripe-gradient-default",
-        "bg-stripe-gradient-primary",
-        "bg-stripe-gradient-secondary",
-        "bg-stripe-gradient-success",
-        "bg-stripe-gradient-warning",
-        "bg-stripe-gradient-danger",
-      ],
-    },
+  theme: {
+    spacing: ["divider"],
+    radius: COMMON_UNITS,
+  },
+  classGroups: {
+    shadow: [{shadow: COMMON_UNITS}],
+    opacity: [{opacity: ["disabled"]}],
+    "font-size": [{text: ["tiny", ...COMMON_UNITS]}],
+    "border-w": [{border: COMMON_UNITS}],
+    "bg-image": [
+      "bg-stripe-gradient-default",
+      "bg-stripe-gradient-primary",
+      "bg-stripe-gradient-secondary",
+      "bg-stripe-gradient-success",
+      "bg-stripe-gradient-warning",
+      "bg-stripe-gradient-danger",
+    ],
   },
 };
