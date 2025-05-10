5db18259c0ff34a6ab39c9e591b14db6fc81e0a7
KumJungMin <rmawjdals@naver.com>
2025-05-10 14:03:50 +0900

    fix(autocomplete): ensure focused item matches selected item after filter, selection

--- packages/components/autocomplete/src/use-autocomplete.ts
+++ packages/components/autocomplete/src/use-autocomplete.ts
@@ -339,15 +339,26 @@ export function useAutocomplete<T extends object>(originalProps: UseAutocomplete
     }
   }, [inputRef.current]);
 
-  // focus first non-disabled item
+  // focus selected item or first non-disabled item
   useEffect(() => {
-    let key = state.collection.getFirstKey();
+    let keyToFocus;
 
-    while (key && state.disabledKeys.has(key)) {
-      key = state.collection.getKeyAfter(key);
+    if (
+      state.selectedKey != null &&
+      state.collection.getItem(state.selectedKey) &&
+      !state.disabledKeys.has(state.selectedKey)
+    ) {
+      keyToFocus = state.selectedKey;
+    } else {
+      let firstAvailableKey = state.collection.getFirstKey();
+
+      while (firstAvailableKey && state.disabledKeys.has(firstAvailableKey)) {
+        firstAvailableKey = state.collection.getKeyAfter(firstAvailableKey);
+      }
+      keyToFocus = firstAvailableKey;
     }
-    state.selectionManager.setFocusedKey(key);
-  }, [state.collection, state.disabledKeys]);
+    state.selectionManager.setFocusedKey(keyToFocus);
+  }, [state.collection, state.disabledKeys, state.selectedKey]);
 
   useEffect(() => {
     if (isOpen) {
