538a15c30a3ac2e3ddee01733add81be9168d579
Vishv Salvi <82429084+Vishvsalvi@users.noreply.github.com>
2025-06-07 10:58:29 +0530

    Update carbon-ad.tsx

--- apps/docs/components/ads/carbon-ad/carbon-ad.tsx
+++ apps/docs/components/ads/carbon-ad/carbon-ad.tsx
@@ -2,7 +2,7 @@
 
 import React, {useCallback, useEffect} from "react";
 import Script from "next/script";
-import {Image} from "@heroui/react";
+
 
 import carbonOptimize from "./carbon-optimize";
 
@@ -11,14 +11,6 @@ import {useIsMounted} from "@/hooks/use-is-mounted";
 import {__PROD__, __ENABLE_ADS__} from "@/utils";
 
 const EA_PROVIDER_RATIO = 0.85;
-const INTERNAL_AD_ENABLED = false;
-
-const INTERNAL_AD_INFO = {
-  description: "Join our hackathon, $6k in prizes + 3 months of HeroUI Chat Pro! ↗",
-  title: "Hackathon #HeroHack",
-  img: "https://heroui-chat-assets.nyc3.cdn.digitaloceanspaces.com/herohack.png",
-  url: "https://hack.heroui.chat/twitter?utm_source=heroui.com&utm_medium=ad-banner",
-};
 
 export const CarbonAd: React.FC<unknown> = () => {
   const carbonRef = React.useRef(null);
@@ -50,7 +42,7 @@ export const CarbonAd: React.FC<unknown> = () => {
   }, []);
 
   useEffect(() => {
-    if (INTERNAL_AD_ENABLED) return;
+ 
 
     const shouldShowEthicalAds = Math.random() < EA_PROVIDER_RATIO;
 
@@ -106,34 +98,6 @@ export const CarbonAd: React.FC<unknown> = () => {
       loadCarbon && clearTimeout(loadCarbon);
     };
   }, [isMounted]);
-
-  if (INTERNAL_AD_ENABLED) {
-    return (
-      <div className="px-2 not-prose hover:opacity-80 transition-[opacity] duration-200 carbon-ad-container max-h-[100px] min-h-[100px] h-[100px] m-0 p-0">
-        <a
-          className="group flex items-center gap-4 h-full"
-          href={INTERNAL_AD_INFO.url}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            removeWrapper
-            alt="Product Hunt"
-            className="m-0 w-[120px] h-[80px] object-cover rounded-[8px]"
-            src={INTERNAL_AD_INFO.img}
-          />
-          <div className="flex flex-col gap-0.5 pointer-events-none">
-            <div className="text-small md:text-medium font-medium no-underline">
-              {INTERNAL_AD_INFO.title}
-            </div>
-            <div className="text-tiny md:text-small text-default-500">
-              {INTERNAL_AD_INFO.description}
-            </div>
-          </div>
-        </a>
-      </div>
-    );
-  }
 
   if (!__PROD__ || !__ENABLE_ADS__) return null;
 
