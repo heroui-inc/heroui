3fe7a82b28add1117fc787b67452be43ad2adc51
Maharshi Alpesh <maharshialpesh@gmail.com>
2025-07-03 15:12:19 +0530

    chore: adding the animations to the tabs

--- packages/react/src/components/tabs/tabs.styles.ts
+++ packages/react/src/components/tabs/tabs.styles.ts
@@ -17,13 +17,21 @@ export const tabsVariants = tv({
     ],
     tab: [
       // Base styles
-      "relative h-7 cursor-default outline-none",
+      "relative w-full cursor-default cursor-pointer rounded-md text-center font-medium outline-none",
       // Orientation styles
       "group-data-[orientation=horizontal]:px-3 group-data-[orientation=horizontal]:py-1",
       "group-data-[orientation=vertical]:px-4 group-data-[orientation=vertical]:py-2",
       // Disabled state
       "data-[disabled=true]:opacity-disabled",
       "data-[disabled=true]:cursor-not-allowed",
+      // hover state
+      "hover:opacity-70 data-[selected=true]:hover:opacity-100",
+      // pressed state
+      "data-[pressed=true]:scale-[0.98]",
+      // transform styles
+      "transform-gpu",
+      // transition styles
+      "transition-[background-color,text-color] duration-300 ease-out",
       // Focus state
       focusRingClasses,
     ],
@@ -48,18 +56,18 @@ export const tabsVariants = tv({
     variant: {
       default: {
         tab: [
-          // Base styles
-          "w-full rounded-md text-center",
-          "hover:opacity-70 data-[selected=true]:hover:opacity-100",
-          // Selected state
-          "data-[selected=true]:bg-background data-[selected=true]:text-foreground data-[selected=true]:shadow-border",
+          // selected state
+          "data-[selected=true]:bg-background data-[selected=true]:text-foreground data-[selected=true]:shadow-border bg-transparent",
         ],
         tabList: "bg-surface text-surface-foreground/70 rounded-lg p-1",
       },
       line: {
-        tab: "data-[selected=true]:bg-surface rounded-md bg-transparent",
+        tab: [
+          // selected state
+          "data-[selected=true]:bg-surface bg-transparent",
+        ],
         tabList: [
-          "bg-surface2 rounded-lg p-1",
+          "bg-surface2 p-1",
           "data-[orientation=horizontal]:border-b",
           "data-[orientation=vertical]:border-r",
         ],
