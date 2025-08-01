9c03e31210a84f3d2c8e893ec77214b470f13301
Junior Garcia <jrgarciadev@gmail.com>
2025-08-01 19:18:14 -0300

    chore: bump versions for react and styles to alpha 12

--- packages/react/src/components/link/link.stories.tsx
+++ packages/react/src/components/link/link.stories.tsx
@@ -3,6 +3,7 @@ import type {Meta} from "@storybook/react";
 
 import React from "react";
 
+import {buttonVariants} from "../button/index";
 import {LinkIcon} from "../icons";
 
 import {Link} from "./index";
@@ -22,7 +23,8 @@ const Template = (_props: LinkProps) => (
       Call to action <LinkIcon className="text-muted absolute -right-2 top-1" />
     </Link>
     <Link
-      className="button button--md button--tertiary relative h-8 pr-5"
+      // className="button button--md button--tertiary relative h-8 pr-5"
+      className={buttonVariants({className: "relative h-8 pr-5", size: "md", variant: "tertiary"})}
       href="https://heroui.com"
       rel="noopener noreferrer"
       target="_blank"
