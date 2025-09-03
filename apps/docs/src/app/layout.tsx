7d57a662c69bdbdc876fc23646a625ab5aa34912
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2025-09-03 16:55:51 +0800

    feat: favicon

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,7 +1,10 @@
+import type {Metadata} from "next";
 import type {ReactNode} from "react";
 
 import {RootProvider} from "fumadocs-ui/provider";
 import {Inter} from "next/font/google";
+
+import {siteConfig} from "@/config/site";
 
 import "./global.css";
 
@@ -18,3 +21,44 @@ export default function Layout({children}: {children: ReactNode}) {
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      media: "(prefers-color-scheme: light)",
+      rel: "icon",
+      type: "image/svg",
+      url: "/icons/favicon-dark.svg",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      rel: "icon",
+      type: "image/svg",
+      url: "/icons/favicon.svg",
+    },
+    {
+      rel: "apple-touch-icon",
+      type: "image/png",
+      url: "/icons/apple-touch-icon.png",
+    },
+  ],
+  keywords: [
+    "React",
+    "Next.js",
+    "NextUI",
+    "Tailwind CSS",
+    "HeroUI",
+    "React Aria",
+    "Server Components",
+    "React Components",
+    "UI Components",
+    "UI Kit",
+    "UI Library",
+    "UI Framework",
+    "UI Design System",
+  ],
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+};
