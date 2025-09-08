f3927a179939959ccb2f7a6b0ed3ca93e758dfc1
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2025-09-09 02:47:32 +0800

    fix: theme of storybook docs

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,14 +1,15 @@
 import type {Preview} from "@storybook/react";
 
+import {addons} from "@storybook/preview-api";
 import {configureActions} from "@storybook/addon-actions";
 import {themes} from "@storybook/theming";
-import React from "react";
+import {DocsContainer} from '@storybook/blocks';
+import React, { useEffect, useState } from "react";
+import {DARK_MODE_EVENT_NAME,UPDATE_DARK_MODE_EVENT_NAME} from 'storybook-dark-mode';
 
 import {withInternationalization} from "./addons/i18n/decorator";
 import {withReactScan} from "./addons/react-scan/decorator";
 import {withReactStrictMode} from "./addons/strict-mode/decorator";
-import {withTheme} from "./addons/theme/decorator";
-import {themeGlobalType} from "./addons/theme/preview";
 
 import "../styles/globals.css";
 
@@ -16,48 +17,72 @@ configureActions({
   depth: 200,
 });
 
+const channel = addons.getChannel()
+
+const ThemeContainer = (props) => {
+  const [curTheme, setCurTheme] = useState<string>()
+
+  useEffect(() => {
+    channel.on(DARK_MODE_EVENT_NAME, (value) => {
+      setCurTheme(value ? 'dark' : 'light')
+    })
+  }, [])
+
+  return (
+    <DocsContainer
+      theme={curTheme === 'dark' ? themes.dark : themes.light}
+      context={props.context}
+    >
+      {props.children}
+    </DocsContainer>
+  );
+};
+
 const parameters: Preview["parameters"] = {
+  layout: "fullscreen",
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-  // TODO: Docs dark mode
-  darkMode: {
-    classTarget: "html",
-    current: "light",
-    dark: {
-      ...themes.dark,
-      appBg: "#161616",
-      appBorderRadius: 14,
-      appContentBg: "black",
-      background: "black",
-      barBg: "black",
-      brandTitle: `<img src="/logo-light.svg" style="width: 120px; height: auto;"/>`
-    },
-    darkClass: "dark",
-    light: {
-      ...themes.light,
-      appBorderRadius: 14,
-      brandTitle: `<img src="/logo-dark.svg" style="width: 120px; height: auto;"/>`
-    },
-    lightClass: "light",
-    stylePreview: true,
-  },
-  layout: "fullscreen",
   options: {
     storySort: {
       method: "alphabetical",
       order: ["Foundations", "Components"],
     },
   },
+  darkMode: {
+    classTarget: "html",
+    current: "system",
+    darkClass: "dark",
+    lightClass: "light",
+    stylePreview: true,
+    dark: {
+      ...themes.dark,
+      appBg: "#1B1B1B",
+      appContentBg: "#1B1B1B",
+      background: "#1B1B1B",
+      barBg: "#1B1B1B",
+      brandTitle: `<img src="/logo-light.svg" style="width: 120px; height: auto;"/>`
+    },
+    light: {
+      ...themes.light,
+      appBg: "#FCFCFC",
+      appContentBg: "#FCFCFC",
+      background: "#FCFCFC",
+      barBg: "#FCFCFC",
+      brandTitle: `<img src="/logo-dark.svg" style="width: 120px; height: auto;"/>`
+    },
+  },
+  docs: {
+    container: ThemeContainer
+  }
 };
 
 const decorators: Preview["decorators"] = [
   withReactScan,
   withReactStrictMode,
-  withTheme,
   withInternationalization,
   (Story) => {
     return (
@@ -71,9 +96,6 @@ const decorators: Preview["decorators"] = [
 const preview: Preview = {
   decorators,
   parameters,
-  globalTypes: {
-    ...themeGlobalType,
-  },
   tags: ["autodocs"],
 };
 
