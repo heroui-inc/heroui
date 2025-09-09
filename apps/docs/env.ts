8b7a4329bffcbb56cbb99cb67a364fa42aeb6f50
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2025-09-10 05:10:45 +0800

    fix(docs): base url of preview and production

--- apps/docs/env.ts
+++ apps/docs/env.ts
@@ -4,12 +4,10 @@ import {z} from "zod";
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_APP_ENV: z.enum(["production", "preview", "development"]).default("development"),
-    NEXT_PUBLIC_BASE_DOMAIN_NAME: z.string().min(1),
     NEXT_PUBLIC_CDN_URL: z.url().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_ENV: process.env["NEXT_PUBLIC_APP_ENV"],
-    NEXT_PUBLIC_BASE_DOMAIN_NAME: process.env["NEXT_PUBLIC_BASE_DOMAIN_NAME"],
     NEXT_PUBLIC_CDN_URL: process.env["NEXT_PUBLIC_CDN_URL"],
     NODE_ENV: process.env["NODE_ENV"],
   },
