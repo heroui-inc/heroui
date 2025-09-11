d0f05cb07730c6120cb5206e4aa540683620a5a0
WK Wong <wingkwong.code@gmail.com>
2025-09-12 00:33:33 +0800

    feat(docs): add env keys

--- apps/docs/env.ts
+++ apps/docs/env.ts
@@ -7,11 +7,15 @@ export const env = createEnv({
     NEXT_PUBLIC_CDN_URL: z.url().min(1),
   },
   runtimeEnv: {
+    LOOPS_API_ENDPOINT: process.env["LOOPS_API_ENDPOINT"],
+    LOOPS_API_KEY: process.env["LOOPS_API_KEY"],
     NEXT_PUBLIC_APP_ENV: process.env["NEXT_PUBLIC_APP_ENV"],
     NEXT_PUBLIC_CDN_URL: process.env["NEXT_PUBLIC_CDN_URL"],
     NODE_ENV: process.env["NODE_ENV"],
   },
   server: {
+    LOOPS_API_ENDPOINT: z.string().min(1),
+    LOOPS_API_KEY: z.string().min(1),
     NODE_ENV: z.enum(["development", "production"]).default("development"),
   },
 });
