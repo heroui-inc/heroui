d2a52f74d5022c915ad6b3cdf2ccac88ac97c3af
Junior Garcia <jrgarciadev@gmail.com>
2025-10-25 16:46:46 -0300

    feat: update Kbd component styles and add Abbr support in stories

--- packages/react/src/components/kbd/kbd.stories.tsx
+++ packages/react/src/components/kbd/kbd.stories.tsx
@@ -19,6 +19,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => (
     <Kbd.Root>
+      <Kbd.Abbr keyValue="command" />
       <Kbd.Content>K</Kbd.Content>
     </Kbd.Root>
   ),
