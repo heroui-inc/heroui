d3b9f5162854d10c8d1dbb179689303f9160648c
WK <wingkwong.code@gmail.com>
2025-10-28 01:39:25 +0800

    chore(theme): lint

--- packages/core/theme/src/utilities/animation.ts
+++ packages/core/theme/src/utilities/animation.ts
@@ -1,7 +1,7 @@
 export default {
   /** Animation Utilities */
   ".spinner-bar-animation": {
-    animation: "fade-out 1.2s linear infinite ",
+    animation: "fade-out 1.2s linear infinite",
     "animation-delay": "calc(-1.2s + (0.1s * var(--bar-index)))",
     transform: "rotate(calc(30deg * var(--bar-index))) translate(140%)",
   },
