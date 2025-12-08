d854120f490fecaf42ea52668a1e0d458b1bc84d
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2025-12-09 03:21:08 +0800

    refactor(storybook): migrating to 10

--- packages/storybook/.storybook/addons/theme/preview.ts
+++ packages/storybook/.storybook/addons/theme/preview.ts
@@ -1,4 +1,6 @@
-import {DEFAULT_THEME, THEME_GLOBAL_TYPE_ID, THEME_OPTIONS} from "./constants";
+import {DEFAULT_THEME, THEME_GLOBAL_TYPE_ID, THEME_OPTIONS, THEME_VALUES} from "./constants";
+
+export {THEME_GLOBAL_TYPE_ID};
 
 export const themeGlobalType = {
   [THEME_GLOBAL_TYPE_ID]: {
@@ -7,11 +9,12 @@ export const themeGlobalType = {
     defaultValue: DEFAULT_THEME,
     toolbar: {
       icon: "paintbrush",
-      items: THEME_OPTIONS.map((option) => ({
-        value: option.value,
-        title: option.title,
-        left: option.value === "system" ? "🖥️" : undefined,
-      })),
+      items: THEME_OPTIONS.filter((option) => THEME_VALUES.includes(option.value as any)).map(
+        (option) => ({
+          value: option.value,
+          title: option.title,
+        }),
+      ),
       showName: true,
       dynamicTitle: true,
     },
