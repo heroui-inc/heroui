f6fa3758ceef0de5ed9087589b75e7fbbc5cf0fe
WK Wong <wingkwong.code@gmail.com>
2026-01-15 14:07:09 +0800

    chore(react): add direct exports from react-aria-components

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -17,3 +17,8 @@ export * from "./hooks";
 //  Utils
 //  ===================================
 export {tv, cn, type VariantProps} from "tailwind-variants";
+//  ===================================
+//  React Aria
+//  ===================================
+export {isRTL, RouterProvider, useLocale, useFilter, I18nProvider} from "react-aria-components";
+export {getLocalizationScript} from "react-aria-components/i18n";
