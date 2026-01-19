5f911ffcc706453061e047524a0328dade4311f1
Junior <jrgarciadev@gmail.com>
2026-01-19 19:13:57 -0300

    fix(toast): update default gap between toasts from 14 to 8

--- packages/react/src/components/toast/constants.ts
+++ packages/react/src/components/toast/constants.ts
@@ -1,5 +1,5 @@
 // Default gap between toasts
-export const DEFAULT_GAP = 14;
+export const DEFAULT_GAP = 8;
 // The default maximum number of visible toasts.
 export const DEFAULT_MAX_VISIBLE_TOAST = 3;
 // The default scale factor for toasts.
