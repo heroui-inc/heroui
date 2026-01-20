c0c472a6b430f31920e1754009c872ec4a3a902b
WK Wong <wingkwong.code@gmail.com>
2026-01-21 00:02:23 +0800

    chore(docs): update example

--- apps/docs/src/demos/button/ripple-effect.tsx
+++ apps/docs/src/demos/button/ripple-effect.tsx
@@ -7,7 +7,7 @@ import "m3-ripple/ripple.css";
 
 export function RippleEffect() {
   return (
-    <Button className="relative bg-[#6200ee] px-8 py-4 text-white">
+    <Button variant="secondary">
       <Ripple />
       Click me
     </Button>
