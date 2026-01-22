6f3f0e4c9326bc1dc78770fc39f4c7127521d281
Junior <jrgarciadev@gmail.com>
2026-01-22 10:46:11 -0300

    refactor(styles): rename CSS classes to hyphenated format

--- packages/styles/src/components/listbox-section/listbox-section.styles.ts
+++ packages/styles/src/components/listbox-section/listbox-section.styles.ts
@@ -3,7 +3,7 @@ import type {VariantProps} from "tailwind-variants";
 import {tv} from "tailwind-variants";
 
 export const listboxSectionVariants = tv({
-  base: "listbox-section",
+  base: "list-box-section",
 });
 
 export type ListBoxSectionVariants = VariantProps<typeof listboxSectionVariants>;
