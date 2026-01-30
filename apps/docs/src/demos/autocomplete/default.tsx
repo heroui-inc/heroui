17e39564bf0cd6c9c87413c1ae3b392c02562334
WK <wingkwong.code@gmail.com>
2026-01-31 00:52:15 +0800

    chore(docs): revise autocomplete example items (#6179)

--- apps/docs/src/demos/autocomplete/default.tsx
+++ apps/docs/src/demos/autocomplete/default.tsx
@@ -20,12 +20,12 @@ export default function Default() {
   const [selectedKeys, setSelectedKeys] = useState<Key[]>([]);
 
   const items = [
+    {id: "florida", name: "Florida"},
+    {id: "delaware", name: "Delaware"},
     {id: "california", name: "California"},
     {id: "texas", name: "Texas"},
-    {id: "florida", name: "Florida"},
     {id: "new-york", name: "New York"},
-    {id: "illinois", name: "Illinois"},
-    {id: "pennsylvania", name: "Pennsylvania"},
+    {id: "washington", name: "Washington"},
   ];
 
   const onRemoveTags = (keys: Set<Key>) => {
@@ -35,12 +35,12 @@ export default function Default() {
   return (
     <Autocomplete
       className="w-[256px]"
-      placeholder="Select countries"
+      placeholder="Select states"
       selectionMode="multiple"
       value={selectedKeys}
       onChange={(keys: Key | Key[] | null) => setSelectedKeys(keys as Key[])}
     >
-      <Label>Countries to Visit</Label>
+      <Label>States to Visit</Label>
       <Autocomplete.Trigger>
         <Autocomplete.Value>
           {({defaultChildren, isPlaceholder, state}: any) => {
