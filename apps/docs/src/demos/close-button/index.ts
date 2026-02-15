1552837f5e73e34322845b39b4cb60a0228d36f3
WK Wong <wingkwong.code@gmail.com>
2026-02-15 21:05:34 +0800

    chore(docs): remove CustomRenderFunction from close button

--- apps/docs/src/demos/close-button/index.ts
+++ apps/docs/src/demos/close-button/index.ts
@@ -1,4 +1,3 @@
 export {Default} from "./default";
 export {WithCustomIcon} from "./with-custom-icon";
 export {Interactive} from "./interactive";
-export {CustomRenderFunction} from "./custom-render-function";
