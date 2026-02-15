f6cf60518a269eeb2c3f01415e69eedbdd4fa5e4
WK Wong <wingkwong.code@gmail.com>
2026-02-15 23:50:30 +0800

    fix(docs): type issue

--- apps/docs/src/demos/breadcrumbs/custom-render-function.tsx
+++ apps/docs/src/demos/breadcrumbs/custom-render-function.tsx
@@ -5,16 +5,16 @@ import {Breadcrumbs} from "@heroui/react";
 export function CustomRenderFunction() {
   return (
     <Breadcrumbs render={(props) => <ol {...props} data-custom="foo" />}>
-      <Breadcrumbs.Item href="#" render={(props) => <li {...props} data-custom="bar" />}>
+      <Breadcrumbs.Item render={(props) => <li {...(props as any)} data-custom="bar" />}>
         Home
       </Breadcrumbs.Item>
-      <Breadcrumbs.Item href="#" render={(props) => <li {...props} data-custom="bar" />}>
+      <Breadcrumbs.Item render={(props) => <li {...(props as any)} data-custom="bar" />}>
         Products
       </Breadcrumbs.Item>
-      <Breadcrumbs.Item href="#" render={(props) => <li {...props} data-custom="bar" />}>
+      <Breadcrumbs.Item render={(props) => <li {...(props as any)} data-custom="bar" />}>
         Electronics
       </Breadcrumbs.Item>
-      <Breadcrumbs.Item render={(props) => <li {...props} data-custom="bar" />}>
+      <Breadcrumbs.Item render={(props) => <li {...(props as any)} data-custom="bar" />}>
         Laptop
       </Breadcrumbs.Item>
     </Breadcrumbs>
