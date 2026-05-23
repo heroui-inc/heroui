05659e325bd2261fed68a8274802a6cb49ee2452
WK Wong <wingkwong.code@gmail.com>
2026-05-24 01:21:46 +0800

    chore(docs): remove ghost variant

--- apps/docs/src/app/[lang]/(home)/layout.tsx
+++ apps/docs/src/app/[lang]/(home)/layout.tsx
@@ -32,7 +32,7 @@ export default async function Layout({
         {
           children: (
             <div className="flex items-center gap-1.5">
-              <DesignThemeSelector triggerVariant="ghost" />
+              <DesignThemeSelector />
               <GitHubLinkSmall />
             </div>
           ),
