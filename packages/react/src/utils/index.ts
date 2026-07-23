0e9c3b86507bb2c3fb07bbf015257a7b7d6d583a
Tianen Pang <32772271+tianenpang@users.noreply.github.com>
2026-07-24 01:00:45 +0800

    fix(tag-group): rac collection builder compatibility

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -2,6 +2,8 @@ export * from "./calendar";
 export * from "./css";
 export * from "./compose";
 export * from "./collection-prop-injection";
+export * from "./field-slots-gate";
+export * from "./use-has-text-slot";
 export * from "./logger";
 export * from "./dom";
 export * from "./variants";
